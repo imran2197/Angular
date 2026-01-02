f87d60ea647ae09d56d4cbb5ba2ec60f07b60701
Md Imran <samimdimran786@gmail.com>
2026-01-02 14:41:37 +0530

    Shared Module

--- 4-input-output/src/app/app.module.ts
+++ 4-input-output/src/app/app.module.ts
@@ -4,22 +4,21 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { CardComponent } from './shared/card/card.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { AddTaskComponent } from './tasks/add-task/add-task.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/card/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     AddTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
