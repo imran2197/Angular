0d0d54b62c44f1801148e2fd7d918dcdf86fa897
Md Imran <samimdimran786@gmail.com>
2026-01-03 00:22:48 +0530

    investment-calculator project

---
+++ 5-investment-calculator/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,14 @@
+import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { AnnualData } from '../../annual-data.model';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [NgFor, NgIf, CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  @Input() results?: AnnualData[] = [];
+}
