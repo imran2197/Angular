952357523646d6079e437ff7ea519bc8323e5473
Md Imran <samimdimran786@gmail.com>
2026-01-08 01:05:46 +0530

    effect() - setup the subscription for signals and whenever change happens it detects in effect function

--- 6-tickets/src/app/dashboard/server-status/server-status.component.ts
+++ 6-tickets/src/app/dashboard/server-status/server-status.component.ts
@@ -1,9 +1,11 @@
 import {
   Component,
   DestroyRef,
+  effect,
   inject,
   OnDestroy,
   OnInit,
+  signal,
 } from '@angular/core';
 
 @Component({
@@ -15,21 +17,29 @@ import {
 })
 // , OnDestroy
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'unknown' | 'online' | 'offline' = 'offline';
+  // currentStatus: 'unknown' | 'online' | 'offline' = 'offline';
+  currentStatus = signal<'unknown' | 'online' | 'offline'>('offline');
   // private interval?: ReturnType<typeof setInterval>;
   private destroyRef = inject(DestroyRef);
-  constructor() {}
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus());
+    });
+  }
 
   ngOnInit() {
     // this.interval
     const interval = setInterval(() => {
       const random = Math.random();
       if (random < 0.5) {
-        this.currentStatus = 'online';
+        // this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (random < 0.9) {
-        this.currentStatus = 'offline';
+        // this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        // this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
 
