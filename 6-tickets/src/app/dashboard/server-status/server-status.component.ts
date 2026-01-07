ea80528f7fc1930913509c8206935568beefa2fa
Md Imran <samimdimran786@gmail.com>
2026-01-07 10:22:38 +0530

    ngOnDestroy alternative - DestroyRef

--- 6-tickets/src/app/dashboard/server-status/server-status.component.ts
+++ 6-tickets/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,13 +13,16 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit, OnDestroy {
+// , OnDestroy
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'unknown' | 'online' | 'offline' = 'offline';
-  private interval?: ReturnType<typeof setInterval>;
+  // private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
   constructor() {}
 
   ngOnInit() {
-    this.interval = setInterval(() => {
+    // this.interval
+    const interval = setInterval(() => {
       const random = Math.random();
       if (random < 0.5) {
         this.currentStatus = 'online';
@@ -23,9 +32,13 @@ export class ServerStatusComponent implements OnInit, OnDestroy {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
 
-  ngOnDestroy(): void {
-    clearInterval(this.interval);
-  }
+  // ngOnDestroy(): void {
+  //   clearInterval(this.interval);
+  // }
 }
